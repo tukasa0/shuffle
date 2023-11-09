43e7aa40642ccdda0f7924a98147f614df171047
sk2k0 <sk2k0@MSI>
2023-11-09 09:49:39 +0900

    シャッフルの改良

--- shuffle.cpp
+++ shuffle.cpp
@@ -8,37 +8,21 @@ void InitRand()
 	srand((unsigned int)time(NULL));
 }
 
-int InitRand(int &randkeep, int arraysize)
+void Shuffle(int* array1, int arraysize)
 {
-	randkeep = rand() % arraysize;
-	return randkeep;
-}
-
-void InitArray(int* array1, int* array2, int arraysize)
-{
-	for (int i = 0; i < arraysize; ++i)
+	for (int i = arraysize - 1; i > 0; --i)
 	{
-		*(array2 + i) = *(array1 + i);
+		int keep = 0;
+		int j = rand() % i;
+
+		keep = *(array1 + i);
+		*(array1 + i) = *(array1 + j);
+		*(array1 + j) = keep;
 	}
+
 }
 
-void Shuffle(int* array1, int* array2, int arraysize)
-{
-	for (int i = arraysize - 1; 0 <= i; --i)
-	{
-		int randkeep = 0;
-
-		while (*(array2 + InitRand(randkeep, arraysize)) == 0) //中身が0だったらarrayを選びなおす
-		{
-
-		}
-
-		*(array1 + i) = *(array2 + randkeep); //ランダムに選ばれたrandkeep番目をbの後ろから入れる
-		*(array2 + randkeep) = 0;    //選ばれたrandkeep番目に0を入れる
-	}
-}
-
-void Show(int* array1, int arraysize)
+void Show(const int* array1, const int arraysize)
 {
 	for (int i = 0; i < arraysize; ++i)
 	{
@@ -52,26 +36,16 @@ int main()
 
 	const int MAX_INDEX = 10;
 	int array1[MAX_INDEX] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int* array2 = new int[MAX_INDEX];
-
-	if (array2 == NULL)
-	{
-		*array2 = 0;
-		cout << *array2 << endl;
-
-		delete[] array2;
-	}
 
 	cout << "シャッフルする前 " << flush;
 	Show(array1, MAX_INDEX);
 	cout << endl;
 
-	InitArray(array1, array2, MAX_INDEX);
-	Shuffle(array1, array2, MAX_INDEX);
+	Shuffle(array1, MAX_INDEX);
 	
 	cout << "シャッフルした後 " << flush;
 	Show(array1, MAX_INDEX);
 	cout << endl;
 	
-	delete[] array2;
+	
 }
